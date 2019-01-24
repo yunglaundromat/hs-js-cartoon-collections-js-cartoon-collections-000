cee99ddf0da71d813f9781dc99da285a5097c1f2
Nathaniel Cain <nathaniel2897@gmail.com>
2019-01-24 03:14:56 +0000

    Done.

--- index.js
+++ index.js
@@ -1,11 +1,33 @@
 function dwarfRollCall(dwarves) {
+  var rollCall = "";
+  for (let i = 0; i < dwarves.length; i++) {
+    rollCall += [i+1] + `. ` + dwarves[i] + ' '; 
+  }
+  return rollCall;
 }
 
 function summonCaptainPlanet(planeteerCalls){
+  var calls = [];
+  for (let i = 0; i < planeteerCalls.length; i++) {
+   calls.push(planeteerCalls[i].toUpperCase() + '!');
+  }
+  return calls;
 }
 
 function longPlaneteerCalls(words) {
+  for (let i = 0; i < words.length; i++) {
+    if (words[i].length > 4) {
+      return true;
+    }
+  }
+  return false;
 }
 
 function findTheCheese (foods) {
+  for (let i = 0; i < foods.length; i++) {
+    if (foods[i] === "cheddar" || foods[i] === "gouda" || foods[i] === "camembert") {
+      return foods[i];
+    }
+  }
+  return "no cheese!";
 }
